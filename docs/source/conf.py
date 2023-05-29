f4fd71dc48a65e86d073ca0b6d2a500e6f4813dd
Roberto <roberto.pellungrini@di.unipi.it>
2023-05-29 15:16:04 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,9 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
- 'sphinx.ext.autosummary',
-'numpydoc',
+    'sphinx.ext.autosummary',
+    'numpydoc',
+    'sphinx_rtd_theme',
 ]
 numpydoc_show_inherited_class_members = False
 # Add any paths that contain templates here, relative to this directory.
@@ -70,7 +71,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
